bc3653f10894b61649dcebebd169d3ef12900ec0
naipmoro <naipmoro@gmail.com>
2016-01-16 19:00:42 -0500

    Test large :length opt requiring consecutive gets

--- test/naipmoro/qrng/core_test.clj
+++ test/naipmoro/qrng/core_test.clj
@@ -36,29 +36,66 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest qrand-simple-test
- (testing "basic usage"
-   (let [v1 (qrand)
-         v2 (qrand :length 5)
-         v3 (qrand :length 8 :type :int16)
-         v4 (qrand :type :hex16)
-         v5 (qrand :type :hex16 :length 11)
-         v6 (qrand :type :hex16 :length 13 :blocks 2)
-         v7 (qrand :type :hex16 :blocks 1024)
-         v7chars (apply seq v7)]
-     (is (every? #(and (>= % 0) (< % 256)) v1))
-     (is (= 1 (count v1)))
-     (is (every? #(and (>= % 0) (< % 256)) v2))
-     (is (= 5 (count v2)))
-     (is (every? #(and (>= % 0) (< % 65536)) v3))
-     (is (= 8 (count v3)))
-     (is (every? hex16? v4))
-     (is (= 1 (count v4)))
-     (is (every? hex16? v5))
-     (is (= 11 (count v5)))
-     (is (every? hex32? v6))
-     (is (= 13 (count v6)))
-     (is (every? hex-char? v7chars))
-     (is (= 2048 (count v7chars)))
-     (is (= 1 (count v7))))))
+(deftest consecutive-gets-test
+  (testing "basic usage"
+    (let [v1 (qrand)
+          v2 (qrand :length 5001)
+          v3 (qrand :length 8 :type :int16)
+          v4 (qrand :type :hex16)
+          v5 (qrand :type :hex16 :length 11)
+          v6 (qrand :type :hex16 :length 13 :blocks 2)
+          v7 (qrand :type :hex16 :blocks 1024)
+          v7chars (apply seq v7)]
+      (is (every? #(and (>= % 0) (< % 256)) v1))
+      (is (= 1 (count v1)))
+      (is (every? #(and (>= % 0) (< % 256)) v2))
+      (is (= 5001 (count v2)))
+      (is (every? #(and (>= % 0) (< % 65536)) v3))
+      (is (= 8 (count v3)))
+      (is (every? hex16? v4))
+      (is (= 1 (count v4)))
+      (is (every? hex16? v5))
+      (is (= 11 (count v5)))
+      (is (every? hex32? v6))
+      (is (= 13 (count v6)))
+      (is (every? hex-char? v7chars))
+      (is (= 2048 (count v7chars)))
+      (is (= 1 (count v7))))))
+
+(deftest t1
+  (let [v1 (qrand)]
+    (is (every? #(and (>= % 0) (< % 256)) v1))
+    (is (= 1 (count v1)))))
+
+(deftest t2
+  (let [v2 (qrand :length 5001)]
+    (is (every? #(and (>= % 0) (< % 256)) v2))
+    (is (= 5001 (count v2)))))
+
+(deftest t3
+  (let [v3 (qrand :length 8 :type :int16)]
+    (is (every? #(and (>= % 0) (< % 65536)) v3))
+    (is (= 8 (count v3)))))
+
+(deftest t4
+  (let [v4 (qrand :type :hex16)]
+    (is (every? hex16? v4))
+    (is (= 1 (count v4)))))
+
+(deftest t5
+  (let [v5 (qrand :type :hex16 :length 11)]
+    (is (every? hex16? v5))
+    (is (= 11 (count v5)))))
+
+(deftest t6
+  (let [v6 (qrand :type :hex16 :length 13 :blocks 2)]
+    (is (every? hex32? v6))
+    (is (= 13 (count v6)))))
+
+(deftest t7
+  (let [v7 (qrand :type :hex16 :blocks 1024)
+        v7chars (apply seq v7)]
+    (is (every? hex-char? v7chars))
+    (is (= 2048 (count v7chars)))
+    (is (= 1 (count v7)))))
 
